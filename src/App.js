b3fe994c7edbfedaa8cca3ad9ca62ad328f967b1
Messaoudi <messaoudichedi54@yahoo.fr>
2020-09-15 14:22:53 +0200

    checi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Checi from './component/Checi'
 
 function App() {
   return (
     <div className="App">
  <h1>hello world</h1>
+ <Checi/>
     </div>
   );
 }
